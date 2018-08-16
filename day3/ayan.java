816a297e8c42fa8c76fc5bbaf19e8f3d56a2373b
Aayush <sharma.aayushashu@gmail.com>
2018-08-16 20:59:02 +0530

    fist task

---
+++ day3/ayan.java
@@ -0,0 +1,14 @@
+class ayan
+{
+static String Inst;
+String myName;
+public static void main(String args[])
+{
+Inst=  "Chitkara";
+ayan mt=new ayan();
+mt.myName= "xxx";
+System.out.println("college" +Inst);
+
+System.out.println("myName" +mt.myName);
+}
+}
